9adda537f30b5a481a2559c2fd566e8bd09f4930
tu4mo <tu4mo@users.noreply.github.com>
2017-05-07 20:17:05 +0300

    Initial commit of client

---
+++ client/webpack.config.js
@@ -0,0 +1,64 @@
+const isDev = process.env.NODE_ENV !== 'production'
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+const config = {
+  devtool: isDev ? 'eval' : '',
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: [
+                './src/styles/variables.scss',
+                './src/styles/mixins.scss'
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/src/index.html'),
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
+
+if (!isDev) {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      }
+    })
+  )
+}
+
+module.exports = config
