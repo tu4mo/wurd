aa108b46d6b7f7e6fc508887017f4573382bab12
tu4mo <tu4mo@users.noreply.github.com>
2017-05-11 19:53:38 +0300

    Mockup of login form

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
+import Button from '../Button'
 import './Welcome.scss'
 
 class Welcome extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  onEmailChange = e => {
+    this.setState({ email: e.target.value })
+  }
+
+  onPasswordChange = e => {
+    this.setState({ password: e.target.value })
+  }
+
+  onLogInClick = () => {
+    console.log(this.state)
+  }
+
   render () {
     return (
       <div className="welcome">
@@ -10,6 +28,25 @@ class Welcome extends Component {
             The one-word nanoblogging<br />
             social networking service
           </h2>
+          <div className="login">
+            <input
+              className="login__input login__input--email"
+              placeholder="email"
+              onChange={this.onEmailChange}
+              value={this.state.email}
+            />
+            <input
+              className="login__input login__input--password"
+              type="password"
+              onChange={this.onPasswordChange}
+              placeholder="password"
+              value={this.state.password}
+            />
+            <div className="login__buttons">
+              <Button onClick={this.onLogInClick}>Log In</Button>
+              <Button>Sign Up</Button>
+            </div>
+          </div>
         </div>
       </div>
     )
