5586e96e3de024c536d6334aeb5889152208c092
tu4mo <tu4mo@users.noreply.github.com>
2017-05-12 17:48:18 +0300

    Convert to async function

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,42 +5,36 @@ const jwt = require('jsonwebtoken')
 // Require models
 const User = require('../models/User')
 
-const post = (req, res) => {
+const post = async (req, res) => {
   const { email, password } = req.body
+  const errorResponse = { error: 'The user name or password is incorrect' }
 
-  User.findOne({
-    email
-  })
-  .then((user) => {
+  try {
+    const user = await User.findOne({
+      email
+    })
+
     if (!user) {
-      return res.status(401).json({
-        status: 'fail',
-        message: 'User not found'
-      })
+      return res.status(401).json(errorResponse)
     }
 
-    if (!bcrypt.compareSync(password, user.password)) {
-      return res.status(401).json({
-        status: 'fail',
-        message: 'Wrong password'
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (isMatch) {
+      const token = jwt.sign({ sub: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '7 days'
       })
+
+      return res.status(200).json({
+        token
+      })
+    } else {
+      return res.status(401).json(errorResponse)
     }
-
-    const token = jwt.sign({ sub: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '1 days'
-    })
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        token: token
-      }
-    })
-  })
-  .catch((err) => {
+  } catch (err) {
     res.sendStatus(500)
     throw err
-  })
+  }
 }
 
 module.exports = post
