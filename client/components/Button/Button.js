83694a80cdb6eddc824e9503594db67c84241d4a
tu4mo <tu4mo@users.noreply.github.com>
2017-05-27 20:10:05 +0300

    Change Button to PureComponent

--- client/components/Button/Button.js
+++ client/components/Button/Button.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import './Button.scss'
 
-class Button extends Component {
+class Button extends PureComponent {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
