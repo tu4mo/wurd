4fde2691a8d016dafb74adbde4c1a1c413651ea0
tu4mo <tu4mo@users.noreply.github.com>
2017-06-20 22:28:50 +0300

    Sort users

--- client/selectors/users.js
+++ client/selectors/users.js
@@ -3,10 +3,12 @@ export const getAuthenticatedUser = state => state.users[state.auth.username]
 export const getUser = username => state => state.users[username]
 
 export const getUsers = state =>
-  Object.keys(state.users).map(username => ({
-    username,
-    profileUrl: state.users[username].profileUrl
-  }))
+  Object.keys(state.users)
+    .map(username => ({
+      username,
+      profileUrl: state.users[username].profileUrl
+    }))
+    .sort((a, b) => a.username.localeCompare(b.username))
 
 export const isFollowingUsername = username => state => {
   const authenticatedUser = getAuthenticatedUser(state)
