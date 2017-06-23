c75409454ca22d88e2c0a1b21f81d4a6a955fbcf
tu4mo <tu4mo@users.noreply.github.com>
2017-06-23 12:21:15 +0300

    Format

--- client/components/Spinner/Spinner.js
+++ client/components/Spinner/Spinner.js
@@ -2,9 +2,7 @@ import React from 'react'
 import './Spinner.scss'
 
 const Spinner = () => {
-  return (
-    <div className="spinner" />
-  )
+  return <div className="spinner" />
 }
 
 export default Spinner
