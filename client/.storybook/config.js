1cc8af1caf7beb7a5b9910ea775b1e07ef57f7a4
tu4mo <tu4mo@users.noreply.github.com>
2017-07-06 12:02:14 +0300

    Move knobs to right

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,7 +1,12 @@
+import { setOptions } from '@storybook/addon-options';
 import { configure } from '@storybook/react'
 
 function loadStories() {
   require('./stories/index.js')
 }
+
+setOptions({
+  downPanelInRight: true
+})
 
 configure(loadStories, module)
