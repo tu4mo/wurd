3bc9776bc001777ffa75ca1ba223d7796eedec47
tu4mo <tu4mo@users.noreply.github.com>
2017-08-17 18:36:10 +0300

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: ['standard', 'standard-react', 'prettier'],
+  extends: ['standard', 'standard-react', 'prettier', 'prettier/react'],
   parser: 'babel-eslint',
   rules: {
     'jsx-quotes': ['error', 'prefer-double']
