8152c6f9de43297d9c0b239bb2c60a7f810d768b
tu4mo <tu4mo@users.noreply.github.com>
2017-08-18 16:11:39 +0300

    Use babel-loader for wordwrapjs

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const config = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(wordwrapjs)\/).*/,
         loader: 'babel-loader'
       },
       {
