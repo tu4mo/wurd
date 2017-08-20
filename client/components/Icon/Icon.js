ec8ed7701ba925703105d4b2b6d61e1f78eb316a
tu4mo <tu4mo@users.noreply.github.com>
2017-08-20 17:48:04 +0300

    Add ToolBar icons

--- client/components/Icon/Icon.js
+++ client/components/Icon/Icon.js
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import './Icon.scss'
 
+import homeIcon from './svg/home.svg'
 import likeIcon from './svg/like.svg'
 import likedIcon from './svg/liked.svg'
 import menuIcon from './svg/menu.svg'
 import settingsIcon from './svg/settings.svg'
+import usersIcon from './svg/users.svg'
 
 const ICONS = {
+  home: {
+    src: homeIcon,
+    title: 'Home'
+  },
   like: {
     src: likeIcon,
     title: 'Like'
@@ -24,6 +30,10 @@ const ICONS = {
   settings: {
     src: settingsIcon,
     title: 'Settings'
+  },
+  users: {
+    src: usersIcon,
+    title: 'Users'
   }
 }
 
--- client/components/ToolBar/ToolBar.js
+++ client/components/ToolBar/ToolBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getAuthenticatedUser } from '~/selectors/users'
+import Icon from '../Icon'
 import NavItem from '../NavItem'
 import ProfilePhoto from '../ProfilePhoto'
 import './ToolBar.scss'
@@ -9,8 +10,12 @@ import './ToolBar.scss'
 const ToolBar = ({ user }) =>
   user
     ? <div className="tool-bar">
-        <NavItem to="/">Feed</NavItem>
-        <NavItem to="/users">Users</NavItem>
+        <NavItem to="/">
+          <Icon name="home" />
+        </NavItem>
+        <NavItem to="/users">
+          <Icon name="users" />
+        </NavItem>
         <NavItem to={`/${user.username}`}>
           <ProfilePhoto
             url={user.profileUrl}
