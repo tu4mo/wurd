563d1fe3414b666aeb66d5abb99621814bc42434
tu4mo <tu4mo@users.noreply.github.com>
2017-09-14 18:19:38 +0300

    Add ESLint a11y plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,15 @@ module.exports = {
     jest: true,
     node: true
   },
-  extends: ['standard', 'standard-react', 'prettier', 'prettier/react'],
+  extends: [
+    'standard',
+    'standard-react',
+    'prettier',
+    'prettier/react',
+    'plugin:jsx-a11y/recommended'
+  ],
   parser: 'babel-eslint',
+  plugins: ['jsx-a11y'],
   rules: {
     'jsx-quotes': ['error', 'prefer-double'],
     'react/sort-comp': 'error',
