ed4e4644199cac2b3026acf0dfd092c2f9b85630
tu4mo <tu4mo@users.noreply.github.com>
2017-10-08 22:47:02 +0300

    Begin styled-components refactor

--- client/components/Alert/Alert.js
+++ client/components/Alert/Alert.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './Alert.scss'
+import styled from 'styled-components'
 
-const Alert = ({ message }) => <div className="alert">{message}</div>
+const Alert = ({ className, message }) => (
+  <div className={className}>{message}</div>
+)
 
 Alert.propTypes = {
+  className: PropTypes.string,
   message: PropTypes.string
 }
 
-export default Alert
+const StyledAlert = styled(Alert)`
+  background-color: #f2dede;
+  border-radius: var(--radius-sm);
+  color: #a94442;
+  display: flex;
+  justify-content: center;
+  padding: var(--spacing-sm);
+`
+
+export default StyledAlert
