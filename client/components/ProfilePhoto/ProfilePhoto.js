1dd57d104eda5ddcdf94126ab8865cae74c4c260
tu4mo <tu4mo@users.noreply.github.com>
2017-10-14 14:22:18 +0300

    Refactor ProfilePhoto to styled omponent

--- client/components/ProfilePhoto/ProfilePhoto.js
+++ client/components/ProfilePhoto/ProfilePhoto.js
@@ -1,26 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
-import './ProfilePhoto.scss'
+import styled, { css } from 'styled-components'
 
-const ProfilePhoto = ({ isMe, size, url, username }) => (
-  <div
-    className={classnames('profile-photo', {
-      [`profile-photo--${size}`]: size
-    })}
-  >
-    <img
-      alt={username}
-      className="profile-photo__image"
-      src={url}
-      title={username}
-    />
+const ProfilePhoto = ({ className, isMe, size, url, username }) => (
+  <div className={className}>
+    <img alt={username} className="image" src={url} title={username} />
     {isMe && (
-      <a
-        className="profile-photo__change"
-        href="https://www.gravatar.com"
-        target="_blank"
-      >
+      <a className="change" href="https://www.gravatar.com" target="_blank">
         Change Gravatar
       </a>
     )}
@@ -28,10 +14,60 @@ const ProfilePhoto = ({ isMe, size, url, username }) => (
 )
 
 ProfilePhoto.propTypes = {
+  className: PropTypes.string,
   isMe: PropTypes.bool,
   size: PropTypes.oneOf(['small', 'tiny']),
   url: PropTypes.string,
   username: PropTypes.string
 }
 
-export default ProfilePhoto
+//  prettier-ignore
+const StyledProfilePhoto = styled(ProfilePhoto)`
+  border-radius: 50%;
+  display: inline-block;
+  height: 160px;
+  overflow: hidden;
+  position: relative;
+  vertical-align: middle;
+  width: 160px;
+
+  &:hover {
+    .change {
+      visibility: visible;
+    }
+  }
+
+  ${props => props.size === 'small' && css`
+    height: 40px;
+    width: 40px;
+  `}
+
+  ${props => props.size === 'tiny' && css`
+    height: 30px;
+    width: 30px;
+  `}
+
+  .change {
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.8);
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    left: 0;
+    line-height: 1;
+    position: absolute;
+    right: 0;
+    text-align: center;
+    text-decoration: none;
+    top: 0;
+    visibility: hidden;
+  }
+
+  .image {
+    display: block;
+    height: 100%;
+    width: 100%;
+  }
+`
+
+export default StyledProfilePhoto
