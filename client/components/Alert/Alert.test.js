002bfb6ac7377f1839295dc7aea7bd80580dd6a1
tu4mo <tu4mo@users.noreply.github.com>
2017-10-15 13:04:05 +0300

    Update tests

--- client/components/Alert/Alert.test.js
+++ client/components/Alert/Alert.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Alert from './Alert'
 import renderer from 'react-test-renderer'
+import 'jest-styled-components'
 
 it('renders correctly', () => {
   const tree = renderer.create(<Alert message="Hello World!" />).toJSON()
--- client/components/Box/Box.test.js
+++ client/components/Box/Box.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import 'jest-styled-components'
 
 import Box from './Box'
 import BoxSection from './BoxSection'
