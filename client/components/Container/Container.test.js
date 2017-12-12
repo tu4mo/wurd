77f0a2c50521e7421b08356a90f89fbc64b897b1
tu4mo <tu4mo@users.noreply.github.com>
2017-12-12 21:26:07 +0200

    Format

--- client/components/Container/Container.test.js
+++ client/components/Container/Container.test.js
@@ -6,9 +6,13 @@ import Container from './Container'
 
 it('renders correctly', () => {
   const tree1 = renderer.create(<Container>Test</Container>).toJSON()
-  const tree2 = renderer.create(<Container disablePadding>Test</Container>).toJSON()
+  const tree2 = renderer
+    .create(<Container disablePadding>Test</Container>)
+    .toJSON()
   const tree3 = renderer.create(<Container fullHeight>Test</Container>).toJSON()
-  const tree4 = renderer.create(<Container withYPadding>Test</Container>).toJSON()
+  const tree4 = renderer
+    .create(<Container withYPadding>Test</Container>)
+    .toJSON()
 
   expect(tree1).toMatchSnapshot()
   expect(tree2).toMatchSnapshot()
