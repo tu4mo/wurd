f5fd20e5da345792e1c59ead44b8a6d7add07fb0
tu4mo <tu4mo@users.noreply.github.com>
2018-02-12 19:22:21 +0200

    Fix lint

--- stylelint.js.config.js
+++ stylelint.js.config.js
@@ -1,11 +1,7 @@
-const stylelintConfig = require('./stylelint.config')
-
 module.exports = {
-  ...stylelintConfig,
-  extends: [...stylelintConfig.extends, 'stylelint-config-styled-components'],
+  extends: ['stylelint-config-styled-components'],
   processors: ['stylelint-processor-styled-components'],
   rules: {
-    ...stylelintConfig.rules,
     'value-list-max-empty-lines': null
   }
 }
